39bbd20ef4cad40055f57f8a7a244f7f62e84101
Ayinde John <lolaayinde@gmail.com>
2022-05-14 15:28:41 +0100

    added README file to explain details of the API

--- middleware/error.js
+++ middleware/error.js
@@ -1,22 +1,20 @@
-const winston = require('winston');
+const winston = require("winston");
 
 module.exports = function (app) {
-   app.use((req, res, next) => {
-      const error = new Error('Not Found');
-      error.status = 404;
-      next(error);
+	app.use((req, res, next) => {
+		const error = new Error("Not Found");
+		error.status = 404;
+		next(error);
+	});
+	app.use((error, req, res, next) => {
+		winston.error(error.message, error);
+		res.status(error.status || 500);
 
-   })
-   app.use((error, req, res, next) => {
-      winston.error(error.message, error)
-      res.status(error.status || 500);
-
-      res.json({
-         error: {
-            status: error.messtatussage || 500,
-            message: 'Internal Server Error' || error.message,
-         }
-      })
-   })
-
-}
+		res.json({
+			error: {
+				status: error.messtatussage || 500,
+				message: "Internal Server Error" || error.message,
+			},
+		});
+	});
+};
--- startup/routes.js
+++ startup/routes.js
@@ -1,24 +1,20 @@
-const genres = require('../routers/genres');
-const customers = require('../routers/customers');
-const movies = require('../routers/movies');
-const rental = require('../routers/rental');
-const users = require('../routers/users');
-const returns = require('../routers/return');
-const auth = require('../routers/auth');
-const express = require('express');
-
-
+const genres = require("../routers/genres");
+const customers = require("../routers/customers");
+const movies = require("../routers/movies");
+const rental = require("../routers/rental");
+const users = require("../routers/users");
+const returns = require("../routers/return");
+const auth = require("../routers/auth");
+const express = require("express");
 
 module.exports = function routes(app) {
-
-   app.use(express.json());
-   app.use(express.urlencoded({ extended: false }))
-   app.use('/api/genres', genres);
-   app.use('/api/customers', customers);
-   app.use('/api/movies', movies);
-   app.use('/api/rental', rental);
-   app.use('/api/users', users);
-   app.use('/api/returns', returns);
-   app.use('/api/auth', auth);
-
-}
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
+	app.use("/api/genres", genres);
+	app.use("/api/customers", customers);
+	app.use("/api/movies", movies);
+	app.use("/api/rentals", rental);
+	app.use("/api/users", users);
+	app.use("/api/returns", returns);
+	app.use("/api/auth", auth);
+};
